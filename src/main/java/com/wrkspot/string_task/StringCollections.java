419186d67ae9b4bf911c2cfec1c8c08850a29fc2
Kaviya-Saravanan <kaviya.saravanan@wrkspot.com>
2023-01-10 16:38:35 +0530

    New code

--- src/main/java/com/wrkspot/string_task/StringCollections.java
+++ src/main/java/com/wrkspot/string_task/StringCollections.java
@@ -1,10 +1,11 @@
 package com.wrkspot.string_task;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class StringCollection {
+public class StringCollections {
     public static void main(String[] args) {
         String name = "Can I block your calender from 5:30pm to 6pm today to review/discuss on test case scenarios for HK dashboard cards ";
         List<String> n = Arrays.asList(name.split("\u0020"));
